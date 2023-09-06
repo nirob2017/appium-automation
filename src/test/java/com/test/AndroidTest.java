32d7270b1b44ae1e6d04269a4d3e424f4fcacb03
nirob2017 <nirob726@gmail.com>
2023-09-06 11:59:00 +0600

    Add first test

--- src/test/java/com/test/AndroidTest.java
+++ src/test/java/com/test/AndroidTest.java
@@ -8,9 +8,10 @@ import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 import java.net.MalformedURLException;
@@ -18,9 +19,11 @@ import java.net.URL;
 import java.time.Duration;
 
 public class AndroidTest {
-
+    public enum SwipeDirection {
+        HORIZONTAL, VERTICAL
+    }
     @Test
-    public void androidLaunchTest() throws MalformedURLException, InterruptedException {
+    public void androidLaunchTest() throws MalformedURLException {
         UiAutomator2Options options = new UiAutomator2Options();
         options.setPlatformName("Android");
         options.setAutomationName("UIAutomator2");
@@ -28,85 +31,71 @@ public class AndroidTest {
         options.setApp(System.getProperty("user.dir")+"/app/nopstationCart_4.40.apk");
 
         AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), options);
-        Thread.sleep(4000);
-        driver.findElement(AppiumBy.id("btnAccept")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(500));
+        WebElement buttonAccept = wait.until(ExpectedConditions.visibilityOfElementLocated((AppiumBy.id("btnAccept"))));
+        buttonAccept.click();
 
         WebElement elementToSwipe = driver.findElement(AppiumBy.id("rvHomeCategories"));
-        String targetElementText = "tvProductName";
+        String targetElementText = "Electronics";
 
         // Set a maximum number of swipes to avoid infinite looping
-        int maxSwipes = 10;
+        int maxSwipes = 3;
         int swipes = 0;
-
-        while (swipes < maxSwipes) {
-            // Perform a swipe
-            performSwipe(driver, elementToSwipe);
-
-            // Check if the target element is now visible
-            if (isElementVisible(driver, targetElementText)) {
-                // Element found, break the loop
-                break;
-            }
-
+        while (swipes < maxSwipes && !isTextVisible(driver, targetElementText)) {
+            swipe(driver, SwipeDirection.HORIZONTAL, elementToSwipe);
             swipes++;
         }
 
-        driver.findElement(AppiumBy.id("tvProductName")).click();
-        String mobileText = "Nokia Lumia 1020";
+        driver.findElement(MobileBy.AndroidUIAutomator("new UiSelector().text(\"" + targetElementText + "\")")).click();
 
-        boolean found = false;
-        while (!found) {
-            // Check if the element with the specified text is present
-            if (driver.findElements(MobileBy.AndroidUIAutomator("new UiSelector().textContains(\"" + mobileText + "\")")).size() > 0) {
-                found = true;
-                break;
-            }
-
-            // Swipe from bottom to top (adjust the coordinates as needed)
-            Dimension size = driver.manage().window().getSize();
-            int startX = size.width / 2;
-            int startY = (int) (size.height * 0.8);
-            int endY = (int) (size.height * 0.2);
-
-            TouchAction<?> action = new TouchAction<>(driver);
-            action.press(PointOption.point(startX, startY))
-                    .waitAction(WaitOptions.waitOptions(Duration.ofMillis(1000)))
-                    .moveTo(PointOption.point(startX, endY))
-                    .release()
-                    .perform();
+        String mobileText = "Mattress Bedroom";
+        swipes = 1;
+        while (swipes < maxSwipes && !isTextVisible(driver, mobileText)) {
+            swipe(driver, SwipeDirection.VERTICAL);
+            swipes++;
         }
+
         driver.findElement(MobileBy.AndroidUIAutomator("new UiSelector().text(\"" + mobileText + "\")")).click();
-        Thread.sleep(4000);
+        wait.until(ExpectedConditions.visibilityOfElementLocated((AppiumBy.id("btnAddToCart"))));
+        swipe(driver, SwipeDirection.VERTICAL);
         driver.findElement(AppiumBy.id("btnPlus")).click();
-        driver.findElement(AppiumBy.id("tvSelectedAttr")).click();
-        driver.findElements(By.className("android.widget.RadioButton")).get(1).click();
-        Thread.sleep(500);
-        driver.findElement(AppiumBy.id("tvDone")).click();
         driver.findElement(AppiumBy.id("btnAddToCart")).click();
-        Thread.sleep(5000);
         driver.quit();
 
     }
 
-    private static void performSwipe(AndroidDriver driver, WebElement elementToSwipe) {
-        Dimension size = driver.manage().window().getSize();
-        int startX = (int) (size.width * 0.8); // 90% from the right
-        int endX = (int) (size.width * 0.2); // 10% from the right
-        int y = elementToSwipe.getLocation().getY() + elementToSwipe.getSize().getHeight() / 2;
+    public static void swipe(AndroidDriver driver, SwipeDirection direction, WebElement... elements) {
+        if (elements.length > 1) throw new IllegalArgumentException("Multiple elements provided, only one is allowed");
 
-        TouchAction<?> action = new TouchAction<>(driver)
-                .press(PointOption.point(startX, y))
-                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(1000))) // Adjust the duration as needed
-                .moveTo(PointOption.point(endX, y))
+        Dimension size = driver.manage().window().getSize();
+        int startX = (int) (size.width * 0.7);
+        int endX = (int) (size.width * 0.3);
+        int startY = size.height / 2;
+        int endY = size.height / 2; // Declare endY here
+
+        if (direction == SwipeDirection.VERTICAL) {
+            startY = (int) (size.height * 0.7); // Adjust startY for vertical swipe
+            endY = (int) (size.height * 0.3);  // Adjust endY for vertical swipe
+            startX = endX = size.width / 2;
+        }
+
+        if (elements.length == 1) {
+            endY = elements[0].getLocation().getY() + elements[0].getSize().getHeight() / 2;
+        }
+
+        new TouchAction<>(driver)
+                .press(PointOption.point(startX, startY))
+                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(1000)))
+                .moveTo(PointOption.point(endX, endY))
                 .release()
                 .perform();
     }
 
-    // Function to check if an element is visible
-    private static boolean isElementVisible(AndroidDriver driver, String element) {
+    private static boolean isTextVisible(AndroidDriver driver, String expectedText) {
         try {
-            return driver.findElement(AppiumBy.linkText(String.valueOf(element))).isDisplayed();
-        } catch (org.openqa.selenium.NoSuchElementException | org.openqa.selenium.StaleElementReferenceException e) {
+            String pageSource = driver.getPageSource();
+            return pageSource.matches(expectedText);
+        } catch (org.openqa.selenium.StaleElementReferenceException e) {
             return false;
         }
     }
